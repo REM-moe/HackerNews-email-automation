c1931e148251e8713a0216d418dade764c69542e
REM-moe <98999089+REM-moe@users.noreply.github.com>
2022-02-05 17:39:11 +0530

    exceptions
    added exceptions for better user interaction

--- Hackernews_mailguy.py
+++ Hackernews_mailguy.py
@@ -1,5 +1,5 @@
 import requests # http_requests
-from bs4 import BeautifulSoup #web_scrapping
+from bs4 import BeautifulSoup#web_scrapping
 import smtplib # send the mail
 # email body
 from email.mime.multipart import MIMEMultipart 
@@ -40,46 +40,88 @@ def extract_news(url):
         cnt += ((str(i+1) + ' :: ' + tag.text + "\n" + "<br>") if tag.text !='More' else '')
 
     return cnt 
-
-cnt = extract_news ('https://news.ycombinator.com')
-content += cnt
-content += ('<br>--------------------------------------<br>')
-content += ('<br><br> End of message 🦗')
-
-
-# sending the mail
-
-print("Composing email...📝")
-
-# updating your email id
-#important parameters
-
-SERVER = 'smtp.gmail.com' # your smtp server
-PORT  = 587 # your port number
-FROM  = input("enter the FROM Email adress --> [without@gmal.com] 😁 :  ") + "@gmail.com"
-TO = input("enter the TO Email adress --> [without@gmal.com] 😁 :  ") + "@gmail.com" # could be [list of emails]
-print("[🪶 PASSWORD YOU ENTER WILL BE HIDDEN 🪶]")
-PASS = getpass.getpass("enter password for {} --> ".format(FROM)) # password
-
-msg = MIMEMultipart()
-# [dynamic printing because when we send mail having same subject it turns into a convo and not a new mail]
-msg['Subject'] = "✨Top News Stories In HackerNews [Automated Email]✨" + '' + str(now.day) + '-' + str(now.month) + '-' +str(now.year)
-msg[FROM] = FROM
-msg[TO] = TO
-
-msg.attach(MIMEText(content, 'html')) # since we used html text instead of normal text | remeber <b> and <br>
+try:
+    cnt = extract_news ('https://news.ycombinator.com')
+    content += cnt
+    content += ('<br>--------------------------------------<br>')
+    content += ('<br><br> End of message 🦗')
 
 
+    # sending the mail
 
-print('Initialisng Server... 😴')
+    print("Composing email... 📝")
 
-server = smtplib.SMTP(SERVER,PORT)
-# server = smtplib.SMTP SSL('smtp.gmail.com' , 465)
-server.set_debuglevel(0) # 0- no , 1 error is shown in screen
-server.ehlo()
-server.starttls()
+    # updating your email id
+    #important parameters
 
-server.login(FROM,PASS)
-server.sendmail(FROM,TO , msg.as_string())
-print('Email Sent ✔️✨')
-server.quit()
+    SERVER = 'smtp.gmail.com' # your smtp server
+    PORT  = 587 # your port number
+    FROM  = input("enter the FROM Email adress --> [without@gmal.com] 😁 :  ") + "@gmail.com"
+    TO = input("enter the TO Email adress --> [without@gmal.com] 😁 :  ") + "@gmail.com" # could be [list of emails]
+    print("[🪶 PASSWORD YOU ENTER WILL BE HIDDEN 🪶]")
+    PASS = getpass.getpass("enter password for {} --> ".format(FROM)) # password
+
+    msg = MIMEMultipart()
+    # [dynamic printing because when we send mail having same subject it turns into a convo and not a new mail]
+    msg['Subject'] = "✨Top News Stories In HackerNews [Automated Email]✨" + '' + str(now.day) + '-' + str(now.month) + '-' +str(now.year)
+    msg[FROM] = FROM
+    msg[TO] = TO
+
+    msg.attach(MIMEText(content, 'html')) # since we used html text instead of normal text | remeber <b> and <br>
+
+
+
+    print('Initialisng Server... 😴')
+
+    server = smtplib.SMTP(SERVER,PORT)
+    # server = smtplib.SMTP SSL('smtp.gmail.com' , 465)
+    server.set_debuglevel(0) # 0- no , 1 error is shown in screen
+    server.ehlo()
+    server.starttls()
+
+    server.login(FROM,PASS)
+    server.sendmail(FROM,TO , msg.as_string())
+    print('Email Sent ✔️✨')
+    server.quit()
+
+except smtplib.SMTPAuthenticationError:
+
+    print(" This didnt work, please try the following ")
+    print('''1. checking your internet connectivity
+             2. checking if you had eneabled sign in from less secure apps
+             3. if the password/mail id entered is correct
+    ''')
+except smtplib.SMTPConnectError:
+
+    print(" This didnt work, please try the following ")
+    print('''
+
+             1. checking your internet connectivity
+             2. checking if you had eneabled sign in from less secure apps
+             3. if the password/mail id entered is correct
+    ''')
+except requests.exceptions.ConnectionError:
+    print('*'*66)
+    print('*'*66)
+    print('*'*24 +" you are offline! " + '*'*24 )
+    print('*'*66)
+    print('*'*66)
+except TypeError:
+    print('*'*66)
+    print('*'*66)
+    print('*'*24 +" that did not work " + '*'*24 )
+    print('*'*66)
+    print('*'*66)
+except KeyboardInterrupt:
+    
+    print(" exiting... ")
+
+
+    
+
+
+
+    
+    
+
+
